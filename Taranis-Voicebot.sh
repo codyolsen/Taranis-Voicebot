8a186187fb4c5375711c98a35f0b546e2bcc0f6e
jameswood <j@j-w.co>
2017-03-08 17:06:29 +1100

    added TCAS warning

--- Taranis-Voicebot.sh
+++ Taranis-Voicebot.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-echo "Creating folders..."
+echo -n "Creating folders..."
 mkdir -p SOUNDS/en/SYSTEM
-echo "Done!"
+echo "Done"
 echo "Generating sounds..."
-while IFS=';' read location filename words
-do
-    say --file-format=WAVE --data-format=LEI16@32000 --channels=1 -v Daniel -o "$location/$filename" "$words"
-    echo "Generating $location/$filename..."
-done < $1
-echo "Done!"
+IFS=';'
+while read location filename words
+	do
+		echo -n "Generating $location/$filename..."
+		say --file-format=WAVE --data-format=LEI16@32000 --channels=1 -v Daniel -o "$location/$filename" "$words"
+		echo "Done"
+	done < $1
+echo "All finished!"
